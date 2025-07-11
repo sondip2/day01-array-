e8ddfc8a1953167c9673a4f93cb3986365d5182d
sondip2 <sarkarsondip103@gmail.com>
2025-07-11 14:30:30 +0600

    first commit

---
+++ python revised/loop.py
@@ -0,0 +1,16 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    
+    count = {}
+
+    for ch in s:
+        count[ch] = count.get(ch, 0) +1
+
+        for ch in t:
+            if ch not in count:
+                return False
+            count[ch] -= 1
+            if count[ch] == 0:
+                del count[ch]
+                return len(count) == 0
